603924df11d08bce4323d8655b785fd0e4258a5a
Sergio Hernández <30393456+Sergio100401@users.noreply.github.com>
2018-03-15 23:29:22 +0100

    Add files via upload

---
+++ EdadLegal/EdadLegal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EdadLegal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre y pulse Enter:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Escriba su edad y pulse Enter:");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Hola " + nombre + "," + " eres mayor de edad!");
+                 //Se puede añadir un Console.ReadLine(); aquí también.
+            }
+            else
+            {
+                Console.WriteLine("Hola " + nombre + "," + " no eres mayor de edad... :(");
+                 //Se puede añadir un Console.ReadLine(); aquí también.
+            }
+            Console.ReadLine();
+        }
+    }
+}
